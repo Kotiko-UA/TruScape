8b1d96e69a7b5b18869f63bc6c8e76cbde0dac40
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2024-01-20 22:55:32 +0200

    add talewind style

--- crm/src/app/components/active-label.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './active-label.module.css';
-
-type ActiveLabelProp = { children: React.ReactNode };
-
-export default function ActiveLabel({ children }: ActiveLabelProp) {
-  return <span className={styles.label}>{children}</span>;
-}
